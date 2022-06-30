72ba27543c26b1b7c518a2c3674bca4ba4920900
laurabulloch <s1928598@ed.ac.uk>
2022-06-30 10:24:29 +0100

    Initial commit

---
+++ src/test/java/com/verint/todoappapi/ToDoAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verint.todoappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
