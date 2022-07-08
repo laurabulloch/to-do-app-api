5271253e26f21216a5c398fae7a8f231e19f5b5d
laurabulloch <s1928598@ed.ac.uk>
2022-07-08 10:23:06 +0100

    add cross origin

--- src/main/java/com/verint/todoappapi/ToDosController.java
+++ src/main/java/com/verint/todoappapi/ToDosController.java
@@ -3,10 +3,12 @@ package com.verint.todoappapi;
 import com.verint.todoappapi.api.TodosApi;
 import com.verint.todoappapi.model.ToDoDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class ToDosController implements TodosApi {
 
