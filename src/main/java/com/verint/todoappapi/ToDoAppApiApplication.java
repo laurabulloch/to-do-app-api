53e9748113748d440661e58613844396d917dc2c
laurabulloch <s1928598@ed.ac.uk>
2022-07-18 16:00:57 +0100

    delete redundant import

--- src/main/java/com/verint/todoappapi/ToDoAppApiApplication.java
+++ src/main/java/com/verint/todoappapi/ToDoAppApiApplication.java
@@ -3,7 +3,6 @@ package com.verint.todoappapi;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @Generated
 @SpringBootApplication
