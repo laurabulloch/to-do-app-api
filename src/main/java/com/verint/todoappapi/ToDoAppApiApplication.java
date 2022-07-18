1995509c87028d6e8b686ab74d2b511f74b5c769
laurabulloch <s1928598@ed.ac.uk>
2022-07-18 15:28:34 +0100

    configure postgresql build gradle and properties

--- src/main/java/com/verint/todoappapi/ToDoAppApiApplication.java
+++ src/main/java/com/verint/todoappapi/ToDoAppApiApplication.java
@@ -3,6 +3,7 @@ package com.verint.todoappapi;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @Generated
 @SpringBootApplication
