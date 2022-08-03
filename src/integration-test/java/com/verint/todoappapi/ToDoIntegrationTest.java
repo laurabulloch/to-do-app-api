d836fc091383e6094b5e44d083159e4f81402cfd
laurabulloch <s1928598@ed.ac.uk>
2022-08-03 11:26:14 +0100

    use next id in tests to avoid clearing database

--- src/integration-test/java/com/verint/todoappapi/ToDoIntegrationTest.java
+++ src/integration-test/java/com/verint/todoappapi/ToDoIntegrationTest.java
@@ -2,17 +2,19 @@ package com.verint.todoappapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.*;
 
-@DataJpaTest
 class ToDoIntegrationTest {
-        @BeforeEach
+        private static Integer nextId;
+        private static Integer startId;
+        @BeforeAll
         public void setUp() {
                 if(System.getProperty("url") == null){
                         //RestAssured.baseURI = "http://localhost:8080";
@@ -22,50 +24,58 @@ class ToDoIntegrationTest {
                         RestAssured.baseURI = System.getProperty("url");
                 }
 
-                given()
-                        .body(ToDoDTOBuilder.builder().name("Item 1").build())
+                startId = given()
                         .contentType(ContentType.JSON)
-                        .when().post("/to-dos")
-                        .then().assertThat().statusCode(200);
+                        .body(ToDoDTOBuilder.builder().name("Item 1").build())
+                        .when()
+                        .post("/todos")
+                        .then()
+                        .extract().path("id");
+
+                nextId = startId + 1;
+
+        }
+
+        @Test
+        @Order(1)
+        void postShouldReturnNewItem() {
 
                 given()
                         .body(ToDoDTOBuilder.builder().name("Item 2").build())
                         .contentType(ContentType.JSON)
                         .when().post("/to-dos")
-                        .then().assertThat().statusCode(200);
-
-        }
-
-        @Test
-        void getListToDos() {
-                when().get("/to-dos")
                         .then().statusCode(200)
-                        .body("id",hasItems(1, 2))
-                        .body("name", hasItems("Item 1", "Item 2"));
-        }
-        @Test
-        void postShouldReturnNewItem() {
-
-                given()
-                        .body(ToDoDTOBuilder.builder().name("Item 3").build())
-                        .contentType(ContentType.JSON)
-                        .when().post("/to-dos")
-                        .then().statusCode(200)
-                        .body("name", containsString("Item 3"));
+                        .body("name", containsString("Item 2"));
 
         when().get("/to-dos")
                         .then().statusCode(200)
-                        .body("id",hasItems(2, 3))
-                        .body("name", hasItems("Item 2", "Item 3"));
+                        .body("id",hasItems(nextId))
+                        .body("name", hasItems("Item 1", "Item 2"));
 
         }
+
         @Test
+        @Order(2)
+        void getListToDos() {
+                when().get("/to-dos")
+                        .then().statusCode(200)
+                        .body("id",hasItems(nextId))
+                        .body("name", hasItems("Item 1", "Item 2"));
+        }
+
+        @Test
+        @Order(3)
         void deleteToDos() {
-                when().delete("/to-dos/1")
+                when().delete("/to-dos/" + nextId.toString())
                         .then().statusCode(204);
 
                 when().get("/to-dos")
                         .then().statusCode(200)
-                        .body("id", not(1));
+                        .body("id", not(nextId));
+        }
+        @AfterAll
+        public static void cleanUp(){
+                given()
+                        .when().delete("/to-dos/" + startId.toString());
         }
 }
