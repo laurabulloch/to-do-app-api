188c94ccd9186543e7dab26981f7116f53f7bc80
laurabulloch <s1928598@ed.ac.uk>
2022-08-03 10:39:41 +0100

    pass strictly the test app url

--- src/integration-test/java/com/verint/todoappapi/ToDoIntegrationTest.java
+++ src/integration-test/java/com/verint/todoappapi/ToDoIntegrationTest.java
@@ -12,57 +12,58 @@ import static org.hamcrest.CoreMatchers.*;
 class ToDoIntegrationTest {
         @BeforeEach
         public void setUp() {
-                if(System.getProperty("url") == null){
-                        RestAssured.baseURI = "http://localhost:8080";
-                }
-                else{
+                if (System.getProperty("url") == null) {
+                        //RestAssured.baseURI = "http://localhost:8080";
+                        RestAssured.baseURI = "https://integration-tests.herokuapp.com";
+                } else {
                         RestAssured.baseURI = System.getProperty("url");
+
+
+                        given()
+                                .body(ToDoDTOBuilder.builder().name("Item 1").build())
+                                .contentType(ContentType.JSON)
+                                .when().post("/to-dos")
+                                .then().assertThat().statusCode(200);
+
+                        given()
+                                .body(ToDoDTOBuilder.builder().name("Item 2").build())
+                                .contentType(ContentType.JSON)
+                                .when().post("/to-dos")
+                                .then().assertThat().statusCode(200);
+
                 }
 
-                given()
-                        .body(ToDoDTOBuilder.builder().name("Item 1").build())
-                        .contentType(ContentType.JSON)
-                        .when().post("/to-dos")
-                        .then().assertThat().statusCode(200);
+                @Test
+                void getListToDos() {
+                        when().get("/to-dos")
+                                .then().statusCode(200)
+                                .body("id", hasItems(1, 2))
+                                .body("name", hasItems("Item 1", "Item 2"));
+                }
+                @Test
+                void postShouldReturnNewItem() {
 
-                given()
-                        .body(ToDoDTOBuilder.builder().name("Item 2").build())
-                        .contentType(ContentType.JSON)
-                        .when().post("/to-dos")
-                        .then().assertThat().statusCode(200);
+                        given()
+                                .body(ToDoDTOBuilder.builder().name("Item 3").build())
+                                .contentType(ContentType.JSON)
+                                .when().post("/to-dos")
+                                .then().statusCode(200)
+                                .body("name", containsString("Item 3"));
 
-        }
+                        when().get("/to-dos")
+                                .then().statusCode(200)
+                                .body("id", hasItems(2, 3))
+                                .body("name", hasItems("Item 2", "Item 3"));
 
-        @Test
-        void getListToDos() {
-                when().get("/to-dos")
-                        .then().statusCode(200)
-                        .body("id",hasItems(1, 2))
-                        .body("name", hasItems("Item 1", "Item 2"));
-        }
-        @Test
-        void postShouldReturnNewItem() {
+                }
+                @Test
+                void deleteToDos () {
+                        when().delete("/to-dos/1")
+                                .then().statusCode(204);
 
-                given()
-                        .body(ToDoDTOBuilder.builder().name("Item 3").build())
-                        .contentType(ContentType.JSON)
-                        .when().post("/to-dos")
-                        .then().statusCode(200)
-                        .body("name", containsString("Item 3"));
-
-        when().get("/to-dos")
-                        .then().statusCode(200)
-                        .body("id",hasItems(2, 3))
-                        .body("name", hasItems("Item 2", "Item 3"));
-
-        }
-        @Test
-        void deleteToDos() {
-                when().delete("/to-dos/1")
-                        .then().statusCode(204);
-
-                when().get("/to-dos")
-                        .then().statusCode(200)
-                        .body("id", not(1));
+                        when().get("/to-dos")
+                                .then().statusCode(200)
+                                .body("id", not(1));
+                }
         }
 }
